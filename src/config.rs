152356f5babaf718dadc7576873a6da48edc2b89
RAprogramm <andrey.rozanov.vl@gmail.com>
2025-04-13 18:37:00 +1000

    recursion readin config

--- src/config.rs
+++ src/config.rs
@@ -9,6 +9,7 @@ use git::GitConfig;
 use lsp::LspConfig;
 use nvim_oxi::api::{get_var, set_var};
 use serde::Serialize;
+use serde_json::{Value, from_str, to_value};
 use ui::UiConfig;
 
 use crate::{
@@ -35,76 +36,69 @@ impl NeviraideConfig {
     }
 
     pub fn apply(&self) -> NeviraideResult<()> {
-        self.apply_basic()?;
-        self.apply_git()?;
-        self.apply_lsp()?;
-        self.apply_ui()?;
+        let fields = self.get_fields(self);
+
+        for (key, value) in fields {
+            let full_key = key.clone();
+
+            if let Some(_) = value.as_object() {
+                self.apply_recursive(&full_key, &value)?;
+            } else {
+                self.set_var(&full_key, value)?;
+            }
+        }
         Ok(())
     }
 
-    fn apply_basic(&self) -> NeviraideResult<()> {
-        self.set_var("language", &self.basic.language)?;
-        self.set_var("latest_stable_plugins", self.basic.latest_stable_plugins)?;
-        self.set_var("programming", self.basic.programming.join(","))?;
+    fn apply_recursive<T: Serialize>(&self, prefix: &str, config: &T) -> NeviraideResult<()> {
+        for (key, value) in self.get_fields(config) {
+            let full_key = format!("{}.{}", prefix, key);
+            if let Some(_sub_config) = value.as_object() {
+                self.apply_recursive(&full_key, &value)?;
+            } else {
+                self.set_var(&full_key, value)?;
+            }
+        }
         Ok(())
     }
 
-    fn apply_git(&self) -> NeviraideResult<()> {
-        self.set_var("git.enable_g_signs", self.git.gitsigns.enable_g_signs)?;
-        self.set_var(
-            "git.enable_current_line_blame",
-            self.git.gitsigns.enable_current_line_blame
-        )?;
-        Ok(())
-    }
+    fn get_fields<T: Serialize>(&self, config: &T) -> Vec<(String, Value)> {
+        let mut fields = Vec::new();
 
-    fn apply_lsp(&self) -> NeviraideResult<()> {
-        self.set_var("lsp.format_before_save", self.lsp.format_before_save)?;
-        self.set_var("lsp.inlay_hints", self.lsp.inlay_hints)?;
-        self.set_var("lsp.code_lenses", self.lsp.code_lenses)?;
-        self.set_var("lsp.completion.auto", self.lsp.completion.auto)?;
-        self.set_var(
-            "lsp.diagnostic.enable_d_signs",
-            self.lsp.diagnostic.enable_d_signs
-        )?;
-        self.set_var(
-            "lsp.diagnostic.virtual_text",
-            self.lsp.diagnostic.virtual_text
-        )?;
-        self.set_var(
-            "lsp.diagnostic.show_on_hover",
-            self.lsp.diagnostic.show_on_hover
-        )?;
-        Ok(())
-    }
+        if let Ok(serialized) = to_value(config) {
+            if let Some(object) = serialized.as_object() {
+                for (key, value) in object {
+                    fields.push((key.clone(), value.clone()));
+                }
+            }
+        }
 
-    fn apply_ui(&self) -> NeviraideResult<()> {
-        self.set_var("ui.hyde", self.ui.hyde)?;
-        self.set_var("ui.theme", &self.ui.theme)?;
-        self.set_var("ui.notify", self.ui.notify)?;
-        self.set_var("ui.nonicons", self.ui.nonicons)?;
-        self.set_var("ui.cursor_line", self.ui.cursor_line)?;
-        self.set_var("ui.cursor_column", self.ui.cursor_column)?;
-        self.set_var("ui.indents", self.ui.indents)?;
-        self.set_var(
-            "ui.line_numbers.auto_switch_relative",
-            self.ui.line_numbers.auto_switch_relative
-        )?;
-        self.set_var(
-            "ui.line_numbers.numbers_enabled",
-            self.ui.line_numbers.numbers_enabled
-        )?;
-        self.set_var(
-            "ui.line_numbers.relative_numbers",
-            self.ui.line_numbers.relative_numbers
-        )?;
-        self.set_var("ui.font.family", &self.ui.font.family)?;
-        self.set_var("ui.font.size", self.ui.font.size)?;
-        Ok(())
+        fields
     }
 
     fn set_var<T: ToString + Debug>(&self, key: &str, value: T) -> NeviraideResult<()> {
-        set_var(key, value.to_string()).map_err(|e| {
+        let value_str = value.to_string();
+
+        let value_str = if let Ok(vec) = from_str::<Vec<String>>(&value_str) {
+            let lua_table = format!(
+                "{{{}}}",
+                vec.iter()
+                    .map(|s| format!("\"{}\"", s))
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            );
+            lua_table
+        } else {
+            value_str
+        };
+
+        let value_str = if value_str.starts_with("\"") && value_str.ends_with("\"") {
+            value_str[1..value_str.len() - 1].to_string()
+        } else {
+            value_str
+        };
+
+        set_var(key, value_str).map_err(|e| {
             eprintln!(
                 "Failed to set var: {} with value: {:?}, error: {:?}",
                 key, value, e
@@ -121,17 +115,17 @@ fn test_neviraide_config() -> NeviraideResult<()> {
     config.apply()?;
     println!("config: {config:?}");
 
-    let language: String = get_var("language")?;
+    let language: String = get_var("basic.language")?;
     assert_eq!(language, "ru");
 
-    let latest_stable_plugins: String = get_var("latest_stable_plugins")?;
+    let latest_stable_plugins: String = get_var("basic.latest_stable_plugins")?;
     let latest_stable_plugins = latest_stable_plugins == "true";
     assert_eq!(latest_stable_plugins, true);
 
-    let programming: String = get_var("programming")?;
-    assert_eq!(programming, "lua,rust");
+    let programming: String = get_var("basic.programming")?;
+    assert_eq!(programming, "{\"lua\", \"rust\"}");
 
-    let git_enabled: String = get_var("git.enable_g_signs")?;
+    let git_enabled: String = get_var("git.gitsigns.enable_g_signs")?;
     let git_enabled = git_enabled == "true";
     assert_eq!(git_enabled, true);
 
--- src/config/basic.rs
+++ src/config/basic.rs
@@ -1,5 +1,10 @@
 use std::fmt::{Display, Formatter, Result as FmtResult};
 
+use nvim_oxi::{
+    Object,
+    conversion::{Error as ConversionError, FromObject, ToObject},
+    serde::{Deserializer, Serializer}
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, Serialize, Deserialize)]
@@ -16,6 +21,18 @@ impl BasicConfig {
             latest_stable_plugins: true,
             programming:           vec!["lua".to_string(), "rust".to_string()]
         }
+    }
+}
+
+impl FromObject for BasicConfig {
+    fn from_object(obj: Object) -> Result<Self, ConversionError> {
+        Self::deserialize(Deserializer::new(obj)).map_err(Into::into)
+    }
+}
+
+impl ToObject for BasicConfig {
+    fn to_object(self) -> Result<Object, ConversionError> {
+        self.serialize(Serializer::new()).map_err(Into::into)
     }
 }
 
